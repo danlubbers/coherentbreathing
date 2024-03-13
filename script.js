3173ad8775a88c68af22068b471319967896e0e6
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-12 21:40:29 -0400

    chore: extract js to external script file

---
+++ script.js
@@ -0,0 +1,23 @@
+const title = document.querySelector(".app-title");
+const sphereContainer = document.querySelector(".sphere-container");
+const sphere = document.querySelector(".sphere");
+const caption = document.querySelector(".caption");
+const gong = document.querySelector("#gong");
+
+sphere.addEventListener("click", () => {
+  console.log("sphere.dataset.state", sphere.dataset.state);
+  if (sphere.dataset.state === "stop") {
+    title.setAttribute("style", "visibility: hidden");
+    sphere.dataset.state = "play";
+    sphere.style.animationPlayState = "running";
+    caption.style.setProperty("--animation", "running");
+    gong.play();
+  } else {
+    title.setAttribute("style", "visibility: visible");
+    sphere.dataset.state = "stop";
+    sphere.style.animationPlayState = "paused";
+    caption.style.setProperty("--animation", "paused");
+    gong.pause();
+    // gong.currentTime = 0; // reset audio to beginning
+  }
+});
