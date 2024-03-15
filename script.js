516e3887fca99efd56f751f327fd5b0d78c5eb54
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-15 12:27:34 -0400

    chore(wip): add reset button

--- script.js
+++ script.js
@@ -12,20 +12,31 @@ const sphereContainer = document.querySelector(".sphere-container");
 const sphere = document.querySelector(".sphere");
 const caption = document.querySelector(".caption");
 const gong = document.querySelector("#gong");
+const btnContainer = document.querySelector(".btn-container");
+const resetBtn = document.querySelector(".reset-btn");
 
 sphere.addEventListener("click", () => {
   if (sphere.dataset.state === "stop") {
     title.setAttribute("style", "visibility: hidden");
+    btnContainer.setAttribute("style", "visibility: hidden");
     sphere.dataset.state = "play";
     sphere.style.animationPlayState = "running";
     caption.style.setProperty("--animation", "running");
     gong.play();
   } else {
     title.setAttribute("style", "visibility: visible");
+    btnContainer.setAttribute("style", "visibility: visible");
     sphere.dataset.state = "stop";
     sphere.style.animationPlayState = "paused";
     caption.style.setProperty("--animation", "paused");
     gong.pause();
-    // gong.currentTime = 0; // reset audio to beginning
   }
+});
+
+// broken
+resetBtn.addEventListener("click", () => {
+  sphere.style.animation = "paused";
+
+  gong.pause();
+  gong.currentTime = 0; // reset audio to beginning
 });
