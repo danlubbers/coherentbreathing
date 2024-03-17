222bc7dc71ad9f2388cd6f4cda99ad79e3b0a63b
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-17 19:09:24 -0400

    fix: remove manual service-worker code/files

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js").then(
-    (registration) => {
-      console.log("Service worker registration successful:", registration);
-    },
-    (error) => {
-      console.error(`Service worker registration failed: ${error}`);
-    }
-  );
-} else {
-  console.error("Service workers are not supported.");
-}
-
 const root = document.documentElement;
 const headerContainer = document.querySelector(".header-container");
 const headerTitle = document.querySelector(".header-title");
