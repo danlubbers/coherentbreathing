c2cf1a2179ee4af11455875ad8b2e348ffd7f173
danlubbers <lubbers.dan@gmail.com>
2024-03-17 18:45:45 -0400

    chore: manifest, service-worker and remove dist (#5)
    * fix: build manifest.json with vite
    
    * chore: add dist to gitignore. add module to package.json
    
    * fix: update service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,98 @@
-self.addEventListener("install", function (event) {
+// self.addEventListener("install", function (event) {
+//   event.waitUntil(
+//     caches.open("my-cache").then(function (cache) {
+//       return cache.addAll([
+//         "/",
+//         "index.html",
+//         "base.css",
+//         "style.scss",
+//         "script.js",
+//         "/assets/icon-144x144.png",
+//         "/assets/icon-192x192.png",
+//         "/assets/icon-512x512.png",
+//         "https://s3.amazonaws.com/content.danlubbers.com/audio/coherent_breathing_45min.mp3",
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       if (response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
+
+// The version of the cache.
+const VERSION = "v1";
+
+// The name of the cache
+const CACHE_NAME = `coherent-breathing-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+  "/",
+  "index.html",
+  "base.css",
+  "style.scss",
+  "script.js",
+  "/assets/icon-144x144.png",
+  "/assets/icon-192x192.png",
+  "/assets/icon-512x512.png",
+  "https://s3.amazonaws.com/content.danlubbers.com/audio/coherent_breathing_45min.mp3",
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("my-cache").then(function (cache) {
-      return cache.addAll([
-        "/",
-        "index.html",
-        "base.css",
-        "style.scss",
-        "script.js",
-        "/assets/icon-144x144.png",
-        "/assets/icon-192x192.png",
-        "/assets/icon-512x512.png",
-        "https://s3.amazonaws.com/content.danlubbers.com/audio/coherent_breathing_45min.mp3",
-      ]);
-    })
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })()
   );
 });
 
-self.addEventListener("fetch", function (event) {
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+      await clients.claim();
+    })()
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("/"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      if (response) {
-        return response;
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request.url);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
       }
-      return fetch(event.request);
-    })
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })()
   );
 });
