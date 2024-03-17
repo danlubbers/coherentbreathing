2b9e51aec1170f549de40c98ca04d541afef8a3a
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-17 15:41:27 -0400

    chore: add webpack and toml file for manifest with netlify

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const ManifestPlugin = require("webpack-manifest-plugin");
+
+module.exports = {
+  // ...
+  plugins: [
+    new ManifestPlugin({
+      fileName: "manifest.json",
+    }),
+  ],
+};
