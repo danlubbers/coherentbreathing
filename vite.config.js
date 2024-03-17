11fb88345beaed10f07fc54058ed678f9fb6fbfe
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-17 19:06:50 -0400

    chore: add vite-plugin-pwa package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   build: {
     manifest: true,
   },
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+    }),
+  ],
 });
