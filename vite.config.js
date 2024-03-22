17f5dcc3e0b3620a6ab5e1fc8d5fa878bc505013
Dan Lubbers <lubbers.dan@gmail.com>
2024-03-22 12:38:09 -0400

    fix: forgot to add gstatic url to cache

--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,20 @@ export default defineConfig({
               },
             },
           },
+          {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "gstatic-fonts-cache",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
         ],
       },
     }),
